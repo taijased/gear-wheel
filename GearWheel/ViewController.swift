46e1133dc5641127005c285838305c0052508205
Spiridonov Maxim <taijased@gmail.com>
2021-03-25 18:15:54 +0300

    Initial Commit

---
+++ GearWheel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GearWheel
+//
+//  Created by Максим Спиридонов on 25.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
